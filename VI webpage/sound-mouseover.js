d45b5fa056fa25f9c1bc15d119c168d0b08e2b21
adi2210 <adisatyajit@gmail.com>
2022-06-18 15:18:23 +0530

    added new file

---
+++ VI webpage/sound-mouseover.js
@@ -0,0 +1,58 @@
+
+function playclip1() {
+
+if (document.all)
+ {
+  document.all.sound.src = "home page.mp3";
+ }
+
+else {
+{
+var audio = document.getElementById("a1")[0];
+audio.play();
+}
+}
+}
+function playclip2() {
+
+if (document.all)
+ {
+  document.all.sound.src = "Alphabet.mp3";
+ }
+
+else {
+{
+var audio = document.getElementById("a2")[0];
+audio.play();
+}
+}
+}
+function playclip3() {
+
+if (document.all)
+ {
+  document.all.sound.src = "Music.mp3";
+ }
+
+else {
+{
+var audio = document.getElementById("a3")[0];
+audio.play();
+}
+}
+}
+function playclip4() {
+
+if (document.all)
+ {
+  document.all.sound.src = "composition.mp3";
+ }
+
+else {
+{
+var audio = document.getElementById("a4")[0];
+audio.play();
+}
+}
+}
+
